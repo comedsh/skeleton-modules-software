ef22c10bc051d5c777b02cf37bf756e7ad654896
pengbo <bo.peng@auto007.com>
2015-12-10 10:39:50 +0800

    add ocatalogs

---
+++ auto007_site/auto007_ocatalogs/auto007_ocatalogs_impl/src/main/java/com/fenghua/auto/ocatalogs/backend/service/OcatalogsService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.fenghua.auto.ocatalogs.backend.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OcatalogsService {
+	
+	public List<Map<String, Object>> allBrands();
+}
